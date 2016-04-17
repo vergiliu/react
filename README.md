840c5ad94e7581d3c83719746c122127685ad7f5
vergiliu <vergiliu@gmail.com>
2016-04-17 15:57:07 +0200

    added babel

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # react
 
 - running w/ --save will update the package.json dependencies file
+- npm install -g package, will install it globally available in the system
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
-module.exports = {
-    entry: [  './app/index.js'],
-    module: {
-        loaders: [
-            {test: /\.coffee$/, exclude: /node_modules/, loader: "coffee-loader"}
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 
-        ]
-    }
-}
+module.exports = {
+  entry: ['./app/index.js'],
+  module: {
+    loaders: [
+        {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+    ]
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: "index_bundle.js"
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
